56f58e754dda670c5f4953be28a6231992649980
LopsangLama00 <llopsang123@gmail.com>
2022-08-18 11:11:47 +0545

    Updated

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.index, name="index"),
+   path('',views.index, name='home'),
    path('post_list',views.PostList.as_view(),name="post-list"),
 ]
